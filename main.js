bd59c0c450873836e9a4463240f9b05172484a4b
Eduarda <dudsaraujo16@gmail.com>
2023-07-19 13:54:22 -0300

    Introduce new features

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-import CardGame from "./src/components/CardGame";
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css";
 import './src/styles/settings/colors.css';
+import BoardGame from "./src/objects/BoardGame";
 
 const app = document.querySelector("#app");
-const htmlCardGame = CardGame()
+const htmlBoardGame = BoardGame(6);
 
-app.insertAdjacentHTML("beforeend", htmlCardGame);
+
+app.insertAdjacentHTML("beforeend", htmlBoardGame);
