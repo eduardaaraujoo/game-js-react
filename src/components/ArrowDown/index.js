947fb2ebbe9cd77967a43e26c6f7706321d7f33c
Eduarda <dudsaraujo16@gmail.com>
2023-08-16 10:05:35 -0300

    Create ArrowDown component without CSS

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function ArrowDown() {
+    return`
+    <img src="src/img/IconArrowDown.png" alt="Ícone de uma seta para baixo">
+    `;
+}
+
+
+
+
+export default ArrowDown;
